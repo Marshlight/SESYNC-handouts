aa45cb0079bc7d50e3e2044988d73305be2ae891
Day <Day.Melissa@epa.gov>
2016-09-15 16:01:17 -0400

    sort of finished

--- lesson-6-3.R
+++ lesson-6-3.R
@@ -12,24 +12,29 @@ in1 <- selectInput("pick_species",
                    choices = unique(species[["species_id"]]))
 out1 = textOutput('species_name')
 out2 = plotOutput('species_plot')
-tab <- tabPanel("Species", in1, out1, out2)
-ui <- navbarPage(title = "Portal Project", tab)
+# tab <- tabPanel("Species", in1, out1, out2)      #simple view
+# ui <- navbarPage(title = "Portal Project", tab)
+side <- sidebarPanel("Options", in1)
+main <- mainPanel(out1, out2)
+tab <- tabPanel("Species",
+                sidebarLayout(side, main))
+ui <- navbarPage(title = "Portal Project", tab)  	
 
 # Server
 server <- function(input, output) {
   output[["species_name"]] <- renderText( 
 #     out1 = filter(species, species_id == input[["pick_species"]]) #theoretical but command 
 #     out1 = paste(out1[2:3], collapse = ' ')                       #line doesn't work in shiny!
-    species %>%
-      filter(species_id == input[["pick_species"]]) %>%
-      select(genus, species) %>%
-      paste(collapse = ' ')
+                              species %>%
+                                filter(species_id == input[["pick_species"]]) %>%
+                                select(genus, species) %>%
+                                paste(collapse = ' ')
   )
   output[["species_plot"]] <- renderPlot(
-    surveys %>%
-      filter(species_id == input[["pick_species"]]) %>%
-      ggplot(aes(year)) + 
-      geom_bar()
+                              surveys %>%
+                                filter(species_id == input[["pick_species"]]) %>%
+                                ggplot(aes(year)) + 
+                                geom_bar()
   ) 
 }
 
